63d95ca37018387064a5146ab4aaaeb5c43850de
Vlad Zhichkin <v.zhichkin@yandex.ru>
2020-06-14 00:44:36 +0400

    add widget calendarView

--- app/src/main/java/com/zhivv/dailyplanner/MainActivity.java
+++ app/src/main/java/com/zhivv/dailyplanner/MainActivity.java
@@ -3,6 +3,9 @@ package com.zhivv.dailyplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int dayOfMonth) {
+
+                int mYear = year;
+                int mMonth = month;
+                int mDay = dayOfMonth;
+                String selectedDate = new StringBuilder().append(mMonth + 1)
+                        .append("-").append(mDay).append("-").append(mYear)
+                        .append(" ").toString();
+                Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
+
+            }
+        } );
     }
 }
